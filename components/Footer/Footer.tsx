3b7b549750e443dd1bfa90c8ff96bea021dde71d
v1tyka <racinvictor@gmail.com>
2025-08-26 22:29:12 +0200

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Victor Ratsyn</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app">student@notehub.app</a>
